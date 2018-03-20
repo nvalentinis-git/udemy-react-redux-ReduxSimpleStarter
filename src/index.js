922d14d876a7f98b62974541abe44e1acd3125df
Nestor Valentinis <nesti07@gmail.com>
2018-03-19 21:11:51 -0300

    Nestor - create base components structure

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
 
+import SearchBar from './components/search_bar'
+
+const API_KEY = "AIzaSyCWEPb_oDuHEAwDrvP671J-rlx67F7CZZo";
+
 const App = () => {
-  return <div>Hello</div>;
+  return (
+      <div>
+        <SearchBar />
+      </div>
+  );
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
