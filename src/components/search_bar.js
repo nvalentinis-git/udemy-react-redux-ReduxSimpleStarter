6b795e1987630dfd65c11b9b7fb494345ca82cdb
Nestor Valentinis <nesti07@gmail.com>
2018-03-20 21:18:00 -0300

    Nestor - Add Youtube api call and refactor App Component

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,13 +1,23 @@
 import React, {Component} from 'react';
 
+// Class base Component
 class SearchBar extends Component {
-  render() {
-    return <input onChange={ event => console.log(event.target.value) } />;
+  constructor(props) {
+    super(props);
+
+    this.state = { term: '' };
   }
 
-  // handleInputChange(event) {
-  //   console.log(event.target.value);
-  // }
+  render() {
+    return (
+      <div>
+        <input
+          value={this.state.term}
+          onChange={event => this.setState( {term: event.target.value} )} />;
+      </div>
+    );
+  }
+
 }
 
 export default SearchBar;
