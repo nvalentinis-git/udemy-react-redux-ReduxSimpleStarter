8e7d42647b13e334f8ae50f3b7b40ec72d38d93e
Nestor Valentinis <nesti07@gmail.com>
2018-03-25 19:00:31 -0300

    Nestor - Add lodash Lib to debounce youtoube searches

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom'
 import YTSearch from 'youtube-api-search'
+import _ from 'lodash';
 
 import SearchBar from './components/search_bar'
 import VideoList from './components/video_list'
@@ -31,6 +32,9 @@ class App extends Component {
   }
 
   render() {
+    // wrapp the original functiona and call it once 300 milisecond 
+    const videoSearch = _.debounce((term) => {this.videoSearch(term)}, 300);
+
     return (
         <div>
           <SearchBar onSearchTermChange={term => this.videoSearch(term)}/>
