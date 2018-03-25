30557c2594b4efbb34b4d7439f144584bbf69006
Nestor Valentinis <nesti07@gmail.com>
2018-03-25 18:50:25 -0300

    Nestor - Add callback to SearchBar component to trigger the YT call

--- src/components/video_detail.js
+++ src/components/video_detail.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+// Functional Component
 const VideoDetail = ({video}) => {
 
   if (!video) {
@@ -10,8 +11,6 @@ const VideoDetail = ({video}) => {
   const url = `https://www.youtube.com/embed/${videoId}`; //ES6 String Template
 
   return (
-
-
       <div className="video-detail col-md-8">
         <div className="embed-responsive embed-responsive-16by9">
           <iframe className="embed-responsive-item" src={url}> </iframe>
